9b74f21c5786e7922e8df1d129cb974738377f07
Taslim-M <taslimmahbub@hotmail.com>
2021-04-17 22:07:50 +0400

    New from Lab

---
+++ src/jdbcgui/LoginUser.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jdbcgui;
+
+/**
+ *
+ * @author Tasli
+ */
+public class LoginUser {
+
+    String username;
+    String Name;
+    int type;
+
+    LoginUser(String uuser, String uname, int utype) {
+        username = uuser;
+        Name = uname;
+        type = utype;
+    }
+}
